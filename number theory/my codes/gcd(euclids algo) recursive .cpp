7214f35597da0c2e78444c560597ac5717b770aa
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-08-23 21:21:34 +0600

    Update gcd(euclids algo) recursive .cpp

--- number theory/my codes/gcd(euclids algo) recursive .cpp
+++ number theory/my codes/gcd(euclids algo) recursive .cpp
@@ -1,5 +1,10 @@
 #include<bits/stdc++.h>
-using  namespace std;
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
 
 int gcd(int a,int b)
 {
